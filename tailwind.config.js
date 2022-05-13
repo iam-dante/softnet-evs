0c695fdbfb9235f46a041730f38f869cba7217a3
Brian Temu <bryangasper2124@gmail.com>
2022-05-14 02:29:36 +0300

    redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "evs-blue": {
+          50: "#F4F3FC",
+          100: "#E9E6F9",
+          200: "#CFC9F2",
+          300: "#ADA4EA",
+          400: "#8577DF",
+          500: "#2D2088",
+          600: "#2B1E80",
+          700: "#231867",
+          800: "#181146",
+          900: "#181146",
+        },
+      },
+    },
     fontFamily: {
       sans: ["DM Sans", "sans-serif"],
     },
